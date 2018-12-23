4f305de6eb42a279d3ba299dda5065e194497869
Orly <dadashev@post.bgu.ac.il>
2018-12-23 11:32:42 +0200

    --

--- Server/src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
+++ Server/src/main/java/bgu/spl/net/api/bidi/ConnectionsImpl.java
@@ -1,5 +1,10 @@
+package bgu.spl.net.api.bidi;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import bgu.spl.net.srv.ConnectionHandler;
 
 
 public class ConnectionsImpl<T> implements Connections<T>{
-
+    private Map<Integer,>
 }
