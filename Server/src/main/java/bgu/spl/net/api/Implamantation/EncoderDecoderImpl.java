c9ba880a47f558de79822aa115d4e540ddb7be77
Orly <dadashev@post.bgu.ac.il>
2018-12-29 12:38:07 +0200

    --

--- Server/src/main/java/bgu/spl/net/api/Implamantation/EncoderDecoderImpl.java
+++ Server/src/main/java/bgu/spl/net/api/Implamantation/EncoderDecoderImpl.java
@@ -2,32 +2,43 @@ package bgu.spl.net.api.Implamantation;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
 import java.nio.charset.*;
+import java.util.Arrays;
 
 public class EncoderDecoderImpl implements MessageEncoderDecoder<String> {
-    private byte[] bytes=new byte[1 << 10];
-    private int len=0;
-    public  String decodeNextByte(byte nextByte){
-        //if()
-        return null;
+
+    private byte[] bytes = new byte[1 << 10]; //start with 1k
+    private int len = 0;
+
+    @Override
+    public String decodeNextByte(byte nextByte) {
+        //notice that the top 128 ascii characters have the same representation as their utf-8 counterparts
+        //this allow us to do the following comparison
+        if (nextByte == '\n') {
+            return popString();
+        }
+
+        pushByte(nextByte);
+        return null; //not a line yet
     }
 
-
+    @Override
     public byte[] encode(String message) {
         return (message + "\n").getBytes(); //uses utf8 by default
     }
 
+    private void pushByte(byte nextByte) {
+        if (len >= bytes.length) {
+            bytes = Arrays.copyOf(bytes, len * 2);
+        }
 
-    public byte[] shortToBytes(short num)
-    {
-        byte[] bytesArr = new byte[2];
-        bytesArr[0] = (byte)((num >> 8) & 0xFF);
-        bytesArr[1] = (byte)(num & 0xFF);
-        return bytesArr;
+        bytes[len++] = nextByte;
     }
-    public short bytesToShort(byte[] byteArr)
-    {
-        short result = (short)((byteArr[0] & 0xff) << 8);
-        result += (short)(byteArr[1] & 0xff);
+
+    private String popString() {
+        //notice that we explicitly requesting that the string will be decoded from UTF-8
+        //this is not actually required as it is the default encoding in java.
+        String result = new String(bytes, 0, len, StandardCharsets.UTF_8);
+        len = 0;
         return result;
     }
 }
