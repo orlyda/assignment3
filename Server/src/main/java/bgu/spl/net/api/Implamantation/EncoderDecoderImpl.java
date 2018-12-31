042dc5ac2cdc09b08526575c0e283843fa608cdd
Orly <dadashev@post.bgu.ac.il>
2018-12-31 18:59:33 +0200

    --

--- Server/src/main/java/bgu/spl/net/api/Implamantation/EncoderDecoderImpl.java
+++ Server/src/main/java/bgu/spl/net/api/Implamantation/EncoderDecoderImpl.java
@@ -8,16 +8,36 @@ public class EncoderDecoderImpl implements MessageEncoderDecoder<String> {
 
     private byte[] bytes = new byte[1 << 10]; //start with 1k
     private int len = 0;
+    private byte[] opCode=new byte[2];
+    private int count=0;
 
     @Override
     public String decodeNextByte(byte nextByte) {
         //notice that the top 128 ascii characters have the same representation as their utf-8 counterparts
         //this allow us to do the following comparison
-        if (nextByte == '\0') {
-            return popString();
+        if (nextByte=='\0')
+            count++;
+        if(len>=2)
+        {
+            short s=bytesToShort(opCode);
+            if(s==3||s==7)
+                return String.valueOf(s);
+            if((s==8||s==5)&&nextByte=='\0')
+                return popString();
+            if((s==1||s==2||s==6||s==9)&&count==2){
+                return popString();
+            }
+
         }
         pushByte(nextByte);
         return null; //not a line yet
+    }
+
+    private short bytesToShort(byte[] byteArr)
+    {
+        short result = (short)((byteArr[0] & 0xff) << 8);
+        result += (short)(byteArr[1] & 0xff);
+        return result;
     }
 
     @Override
